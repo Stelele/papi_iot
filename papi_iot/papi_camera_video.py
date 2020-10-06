d5a6d2676bbb894c7c9e44b05461974e0f8bf80b
Stelele <giftmugweni@gmail.com>
2020-10-06 22:15:55 +0100

    Added light adjusting and initiating features for PapiCameraVideo class

--- papi_iot/papi_camera_video.py
+++ papi_iot/papi_camera_video.py
@@ -1,67 +1,147 @@
+from time import sleep
+from picamera import PiCamera
+
+from picamera import PiCameraError
+
 class PapiCameraVideo:
     
     # Class attributes here testing content
-    
     def __init__ (self): 
-	""" Initial state of the object by assigning the values of the object properties
+        """ 
+    
+        Initial state of the object by assigning the values of the object properties
+        
+        """
+        
+        self.camera = PiCamera()
+        
+   
+    def setCameraMode (self, resolution=(1024, 768)): 
+        """ 
+        
+        Switch the camera between infared cut-off and no infared cut-off filter 
+        
+        """
+
+        self.camera.resolution = resolution
+        self.camera.start_preview()
+
+        #camera warm-up time
+        sleep(2)
+        
+    def setLowLightMode (self, alternateISO=False): 
+        """
+        
+        Switch the camera mode to low light mode
+        
+        """
+        
+        self.autoAdjustToLight()
+
+        # Set ISO to low light values
+        self.camera.iso = 800 if alternateISO else 400 
+
+        # wait for the automatic gain control to settle
+        sleep(2)
+
+        # now fix the values
+        self.camera.shutter_speed = self.camera.exposure_speed
+        self.camera.exposure_mode = 'off'
+        
+        gain = self.camera.awb_gains
+        self.camera.awb_mode = 'off'
+        self.camera.awb_gains = gain
+    
+    def setNormalLightMode (self, alternateISO=False): 
+        """
+        
+        Switch the camera mode to normal light mode
+        
+        """
+
+        self.autoAdjustToLight()
+
+        # Set ISO to low light values
+        self.camera.iso = 200 if alternateISO else 100 
+
+        # wait for the automatic gain control to settle
+        sleep(2)
+
+        # now fix the values
+        self.camera.shutter_speed = self.camera.exposure_speed
+        self.camera.exposure_mode = 'off'
+        
+        gain = self.camera.awb_gains
+        self.camera.awb_mode = 'off'
+        self.camera.awb_gains = gain
+        
+    
+    def autoAdjustToLight (self): 
+        """
+        
+        Automatically adjust the camera lighting according to light conditions at the time
+        
+        """
+
+        self.camera.iso = 0
+        self.camera.exposure_mode = 'auto'
+        self.shutter_speed = 0
+        self.camera.awb_mode = 'auto'
+        
+        # wait for automatic gain control to settle
+        sleep(2)
+        
+        
+    
+    def takePhoto (self, name): 
+        """ 
+        
+        Capture the image of face in camera field of view. Note, assumes setCameraMode has been run
+        before running this function 
+        -------------------------------------------------------------------------------------------
+        variables:
+        name: chosen name of photo taken
+
+        """
+        name = name + '.jpg'
+        self.camera.capture(name)
+        
+    
+    def recordVideoFor (self): 
+        """
+        
+        Capture the footage of face in camera field of view
+        
         """
         
         # Add code here
         
         pass
     
-    def setCameraMode (self): 
-	""" Switch the camera between infared cut-off and no infared cut-off filter """
-        
-        # Add code here
-        
-        pass
-        
-    def setLowLightMode (self): 
-	""" Switch the camera mode to low light mode """
-        
-        # Add code here
-        
-        pass
-    
-    def setNormalLightMode (self): 
-	""" Switch the camera mode to normal light mode """
-        
-        # Add code here
-        
-        pass
-    
-    def autoAdjustToLight (self): 
-	""" Automatically adjust the camera lighting according to light conditions at the time """
-        
-        # Add code here
-        
-        pass
-    
-    def takePhoto (self): 
-	""" Capture the image of face in camera field of view """
-        
-        # Add code here
-        
-        pass
-    
-    def recordVideoFor (self): 
-	""" Capture the footage of face in camera field of view """
-        
-        # Add code here
-        
-        pass
-    
     def recordVideo (self): 
-	""" Capture the footage of face in camera field of view """
+        """
+        
+        Capture the footage of face in camera field of view
+        
+        """
         
         # Add code here
         
         pass
     
     def stopVideoRecording (self): 
-	""" Stop the video recording """
+        """
+        
+        Stop the video recording
+        
+        """
         
         # Add code here
         
         pass
+
+if __name__ == "__main__":
+    test = PapiCameraVideo()
+    test.setCameraMode()
+    test.autoAdjustToLight()
+    test.takePhoto("test")
