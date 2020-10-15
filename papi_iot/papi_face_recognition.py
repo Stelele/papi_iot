480f44f80c73cd16ae8837a1602afcac7331c994
OyamaPlati <platioyama@gmail.com>
2020-10-15 02:47:23 +0200

    photo-video-face-detection

--- papi_iot/papi_face_recognition.py
+++ papi_iot/papi_face_recognition.py
@@ -1,170 +1,249 @@
-from PIL import Image, ImageDraw    # for image manipulation
 import face_recognition
 import os
 import cv2
+import pickle
+import time
 
 class PapiFaceRecognition:
     # Class attributes here
-    directories = OfflineStorage()
-    known_faces = []
-    known_names = []
+    #DIRECTORIES = OfflineStorage()
+    KNOWN_FACES_DIR = './home/pi/photos/knownFaces'
+    UNKNOWN_FACES_DIR = './home/pi/photos/unknownFaces'
 
-    def __init__ (self, known_faces_dir, unknown_faces_dir, tolerance=0.6, frame_thickness=3, font_thickness=2, model='cnn'):
+    TOLERANCE = 0.6
+    FRAME_THICKNESS = 3
+    FONT_THICKNESS = 2
+    MODEL = 'cnn'
+
+    VIDEO = cv2.VideoCapture('./home/pi/videos/facerecvideo.mp4') # Could put in a video file or path to video file/stream from camera
+
+    def __init__ (self):
         """
             Initial state of the object by assigning the values of the object’s properties
         """
-        self.known_faces_dir = known_faces_dir
-        self.unknown_faces_dir = unknown_faces_dir
-        self.tolerance = tolerance
-        self.frame_thickness = frame_thickness
-        self.font_thickness = font_thickness
-        self.model = model
+        #self.DIRECTORIES = OfflineStorage()
+        self.KNOWN_FACES_DIR = './home/pi/photos/knownFaces' #DIRECTORIES.getOfflinePhotoStorageLocation('known_faces')
+        self.UNKNOWN_FACES_DIR = './home/pi/photos/unknownFaces' #DIRECTORIES.getOfflinePhotoStorageLocation('unknown_faces')
+        self.TOLERANCE = 0.6
+        self.FRAME_THICKNESS = 3
+        self.FONT_THICKNESS = 2
+        self.MODEL = 'cnn'
+        self.VIDEO = cv2.VideoCapture('./home/pi/videos/facerecvideo.mp4')
 
     def nameToColor (self, name):
+        # Take 3 first letters, tolower()
+        # lowercased character ord() value rage is 97 to 122, substract 97, multiply by 8
         color = [(ord(c.lower()) - 97) * 8 for c in name[:3]]
         return color
+    
+    def faceRecognitionFromPhoto (self):
+        print('Loading known faces...')
+        known_faces = []
+        known_names = []
 
-    def addAllowedUser (self, filename):
-        '''
-            Add the users that the residence-room owner wants to the online
-            database.
+        # We oranize known faces as subfolders of KNOWN_FACES_DIR
+        # Each subfolder's name becomes our label (name)
+        for name in os.listdir(self.KNOWN_FACES_DIR):
 
-            args:
-                image_face (Image): a record of facial images
+            # Next we load every file of faces of known person
+            for filename in os.listdir(f'{self.KNOWN_FACES_DIR}/{name}'):
 
-            return:
-                None
-        '''
+                # Load an image
+                image = face_recognition.load_image_file(f'{self.KNOWN_FACES_DIR}/{name}/{filename}')
 
-        print(f'Filename {filename}')
-        image = face_recognition.load_image_file(f'{filename}')
-        encoding = face_recognition.face_encodings(image)[0]
-        return encoding
+                # Get 128-dimension face encoding
+                # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
+                encoding = face_recognition.face_encodings(image)[0]
 
-    def addBannedUser (self, filename):
-        '''
-            Add the users that are banned from residence-room by the owner to the
-            online database.
-
-            args:
-                image_face (Image): a record of facial images
-
-            return:
-                None
-        '''
-
-        print(f'Filename {filename}')
-        image = face_recognition.load_image_file(f'{filename}')
-        locations = face_recognition.face_locations(image, model=self.model)
-        encodings = face_recognition.face_encodings(image, locations)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        return image, locations, encodings
-
-    def removeUser (self):
-        '''
-            remove the users from the online database.
-
-            args:
-                None
-
-            return:
-                None
-        '''
-
-        # Add code here
-
-        pass
-
-    def identifyFromPhoto (self):
-        '''
-            Find all the faces that appear in a picture. Recognize who appears
-            in each photo
-
-            args:
-
-            return:
-
-        '''
-
-        for name in os.listdir(self.known_faces_dir):
-            for filename in os.listdir(f'{self.known_faces_dir}/{name}'):
-                encoding = addAllowedUser (filename)
+                # Append encodings and name
                 known_faces.append(encoding)
                 known_names.append(name)
 
-        print('Processing unknown faces...')
-        for filename in os.listdir(self.unknown_faces_dir):
-            print(f'Filename {filename}', end='')
-            image, locations, encodings = addBannedUser (filename)
 
+        print('Processing unknown faces...')
+        # Now let's loop over a folder of faces we want to label
+        for filename in os.listdir(self.UNKNOWN_FACES_DIR):
+
+            # Load image
+            print(f'Filename {filename}', end='')
+            image = face_recognition.load_image_file(f'{self.UNKNOWN_FACES_DIR}/{filename}')
+
+            # This time we first grab face locations - we'll need them to draw boxes
+            locations = face_recognition.face_locations(image, model=self.MODEL)
+
+            # Now since we know loctions, we can pass them to face_encodings as second argument
+            # Without that it will search for faces once again slowing down whole process
+            encodings = face_recognition.face_encodings(image, locations)
+
+            # We passed our image through face_locations and face_encodings, so we can modify it
+            # First we need to convert it from RGB to BGR as we are going to work with cv2
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
             print(f', found {len(encodings)} face(s)')
             for face_encoding, face_location in zip(encodings, locations):
-                results = face_recognition.compare_faces(known_faces, face_encoding, self.tolerance)
+
+                # We use compare_faces (but might use face_distance as well)
+                # Returns array of True/False values in order of passed known_faces
+                results = face_recognition.compare_faces(known_faces, face_encoding, self.TOLERANCE)
+
+                # Since order is being preserved, we check if any face was found then grab index
+                # then label (name) of first matching known face withing a tolerance
                 match = None
-                if True in results:
+                if True in results:  # If at least one is true, get a name of first of found labels
                     match = known_names[results.index(True)]
                     print(f' - {match} from {results}')
+
+                    # Each location contains positions in order: top, right, bottom, left
                     top_left = (face_location[3], face_location[0])
                     bottom_right = (face_location[1], face_location[2])
-                    color = nameToColor(match)
-                    cv2.rectangle(image, top_left, bottom_right, color, self.frame_thickness)
+
+                    # Get color by name using our fancy function
+                    color = self.nameToColor(match)
+
+                    # Paint frame
+                    cv2.rectangle(image, top_left, bottom_right, color, self.FRAME_THICKNESS)
+
+                    # Now we need smaller, filled grame below for a name
+                    # This time we use bottom in both corners - to start from bottom and move 50 pixels down
                     top_left = (face_location[3], face_location[2])
                     bottom_right = (face_location[1], face_location[2] + 22)
+
+                    # Paint frame
                     cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-                    cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.5, (200, 200, 200), self.font_thickness)
+
+                    # Wite a name
+                    cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), self.FONT_THICKNESS)
+
+            # Show image
             cv2.imshow(filename, image)
             cv2.waitKey(0)
             cv2.destroyWindow(filename)
 
 
+    def faceRecognitionFromVdeo (self):
+        print('Loading known faces...')
+        known_faces = []
+        known_names = []
 
-    def identifyFromVideo (self):
-        '''
-            Do real-time face recognition
+        # We oranize known faces as subfolders of KNOWN_FACES_DIR
+        # Each subfolder's name becomes our label (name)
+        for name in os.listdir(self.KNOWN_FACES_DIR):
 
-            args:
+            # Next we load every file of faces of known person
+            for filename in os.listdir(f'{self.KNOWN_FACES_DIR}/{name}'):
 
-            return:
+                # Load an image
+                #image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
 
-        '''
+                # Get 128-dimension face encoding
+                # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
+                #encoding = face_recognition.face_encodings(image)[0]
+                encoding = pickle.load(open(f"{self.KNOWN_FACES_DIR}/{name}/{filename}", "rb"))
+                
+                # Append encodings and name
+                known_faces.append(encoding)
+                known_names.append(int(name))
 
-        # Add code here
+        if len(known_names) > 0:
+            next_id = max(known_names) + 1
+        else:
+            next_id = 0
 
-        pass
+        print('Processing unknown faces...')
+        # Now let's loop over a folder of faces we want to label
+        while True:
+        
+            # Load image
+            #print(f'Filename {filename}', end='')
+            #image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
+            ret, image = VIDEO.read()
 
-    def recordTimeOfEncounter (self):
-        '''
-            Do real-time face recognition
+            # This time we first grab face locations - we'll need them to draw boxes
+            locations = face_recognition.face_locations(image, model=self.MODEL)
 
-            args:
-                None
+            # Now since we know loctions, we can pass them to face_encodings as second argument
+            # Without that it will search for faces once again slowing down whole process
+            encodings = face_recognition.face_encodings(image, locations)
 
-            return:
-                hours (integer): hour of the day in real time
-                mins (integer): minutes in real time
-                secs (integer): seconds in real time
-        '''
+            # We passed our image through face_locations and face_encodings, so we can modify it
+            # First we need to convert it from RGB to BGR as we are going to work with cv2
+            #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Add code here
+            # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
+            print(f', found {len(encodings)} face(s)')
+            for face_encoding, face_location in zip(encodings, locations):
 
-        pass
+                # We use compare_faces (but might use face_distance as well)
+                # Returns array of True/False values in order of passed known_faces
+                results = face_recognition.compare_faces(known_faces, face_encoding, self.TOLERANCE)
 
-    def gettingMatchingStatistics (self):
-        '''
-            Output the measure parameters of the matching algorithm
+                # Since order is being preserved, we check if any face was found then grab index
+                # then label (name) of first matching known face withing a tolerance
+                match = None
+                if True in results:  # If at least one is true, get a name of first of found labels
+                    match = known_names[results.index(True)]
+                    print(f' - {match} from {results}')
 
-            args:
-                None
+                else:
+                    match = str(next_id)
+                    next_id = next_id + 1
+                    known_names.append(match)
+                    known_faces.append(face_encoding)
+                    os.mkdir(f"{self.KNOWN_FACES_DIR}/{match}")
+                    pickle.dump(face_encoding, open(f"{self.KNOWN_FACES_DIR}/{match}/{match}-{int(time.time())}.plk", "wb"))
 
-            return:
-                accuracy (float): the percentile match of the recognised face
-                loss (float):  loss is a number indicating how bad the model's prediction was on a single face
-        '''
+                # Each location contains positions in order: top, right, bottom, left
+                top_left = (face_location[3], face_location[0])
+                bottom_right = (face_location[1], face_location[2])
 
-        # Add code here
+                # Get color by name using our fancy function
+                color = nameToColor(match)
 
-        pass
+                # Paint frame
+                cv2.rectangle(image, top_left, bottom_right, color, self.FRAME_THICKNESS)
+
+                # Now we need smaller, filled grame below for a name
+                # This time we use bottom in both corners - to start from bottom and move 50 pixels down
+                top_left = (face_location[3], face_location[2])
+                bottom_right = (face_location[1], face_location[2] + 22)
+
+                # Paint frame
+                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+
+                # Wite a name
+                cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), self.FONT_THICKNESS)
+
+            # Show image
+            cv2.imshow("", image)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            
+            #cv2.destroyWindow(filename)
+
+
+    #def recordTimeOfEncounter (self):
+    #    '''
+    #        Do real-time face recognition
+    #
+    #        args:
+    #            None
+
+    #        return:
+    #            hours (integer): hour of the day in real time
+    #            mins (integer): minutes in real time
+    #            secs (integer): seconds in real time
+    #    '''
+
+    #    # Add code here
+
+    #    pass
 
 if __name__ == "__main__":
-    test = PapiFaceRecognition()
+    unit = PapiFaceRecognition()
+
+    #unit.faceRecognitionFromPhoto()
+    unit.faceRecognitionFromVdeo()
+
+
+    
