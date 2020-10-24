a1ca6290554031a12161762054f3e7af563c9f74
Stelele <giftmugweni@gmail.com>
2020-10-24 02:09:20 +0200

    linked papi and storage classes

--- papi_iot/papi_storage_manager.py
+++ papi_iot/papi_storage_manager.py
@@ -1,19 +1,9 @@
 from papi_storage_online import OnlineStorage
 from papi_storage_offline import OfflineStorage 
 
-class StorageManager(OnlineStorage, OfflineStorage):
+class StorageManager:
     
     def __init__(self):
-        OnlineStorage.__init__(self)
-        OfflineStorage.__init__(self)
-
-        self.useOnlineStorage = False
-        self.useOfflineStorage = True
-
-    def storeOfflinePhotosOnline(self):
-        pass
-
-    def storeOfflineVideosOnline(self):
-        pass
-
+        self.onlineStorage = OnlineStorage()
+        self.offlineStorage = OfflineStorage()
     
