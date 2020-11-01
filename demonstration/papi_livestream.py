6c08eb2cb5842ebf0a45cbc21fd40932c3b661e4
Stelele <giftmugweni@gmail.com>
2020-11-01 12:01:25 +0200

    refactoring

--- demonstration/papi_livestream.py
+++ demonstration/papi_livestream.py
@@ -102,5 +102,5 @@ def check_face_send(newpictureName,oldPicture, sendTo,video_index, skipFor=5):
 
 if __name__ == '__main__':
     from waitress import serve
-    serve (app, host="0.0.0.0", port=8080)  
+    serve (app, host="0.0.0.0", port=8080)   
     
