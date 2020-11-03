dd86d5ede99d801fcf61bc46dd2e6577372bf445
Stelele <giftmugweni@gmail.com>
2020-11-03 16:36:32 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -48,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Stelele/papi_iot',
-    version='3.7.6',
+    version='3.7.7',
     zip_safe=False,
 )
